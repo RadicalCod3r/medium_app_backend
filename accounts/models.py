5beb4f3c465308aa451b40f587da2d049fbea42b
Mahdi <mahdi.zareei.80@gmail.com>
2022-11-11 18:23:24 +0330

    Add RegexField in PhoneNumber

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
@@ -7,7 +8,12 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 class User(AbstractBaseUser, PermissionsMixin):
                     ID = models.IntegerField()
                     email = models.EmailField(max_length=255, unique=True)
-                    phone_number = models.CharField(max_length=11, unique=True)
+                    phone_number = models.CharField(max_length=11, unique=True, validators=[
+                    RegexValidator(
+                        regex=r'^\+?1?\d{9,15}$',
+                        message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+                    ),
+                    ],)
                     first_name = models.CharField(max_length=100)
                     last_name = models.CharField(max_length=100)
                     title = models.CharField(max_length=200)
