d1b772a95f98ce2a22ffa57ce2fa37f62b224c92
Radmehr <radmehrtarnasi135@gmail.com>
2022-11-18 12:14:11 +0330

    modifying user model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from .models import User
+from users.models import User, PhoneOTP
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.forms import UserAdminChangeForm, UserAdminAddForm
 
 # Register your models here.
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminAddForm
+
+    list_display = ('phone', 'name', 'email', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'email', 'password', 'image')}),
+        ('Personal Info', {'fields': ('name', 'logged_in')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')})
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('phone', 'email', 'password1', 'password2')})
+    )
+
+    search_fields = ('phone', 'email', 'name')
+    ordering = ('last_login',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+admin.site.register(PhoneOTP)
---
+++ users/managers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, email, password=None, name=None, is_staff=False, is_superuser=False, is_active=True):
+        if not phone:
+            raise ValueError('You should provide a phone number.')
+        if not email:
+            raise ValueError('You should provide an email address.')
+        if not password:
+            raise ValueError('You should provide a password.')
+        user = self.model(phone = phone, email = email, name = name)
+        user.set_password(password)
+        user.is_staff = is_staff
+        user.is_superuser = is_superuser
+        user.is_active = is_active
+        user.save()
+
+        return user
+
+    def create_staffuser(self, phone, email, password=None):
+        return self.create_user(
+            phone = phone,
+            email = email,
+            password = password,
+            is_staff=True
+        )
+
+    def create_superuser(self, phone, email, password=None):
+        return self.create_user(
+            phone = phone,
+            email = email,
+            password = password,
+            is_staff = True,
+            is_superuser = True
+        )
